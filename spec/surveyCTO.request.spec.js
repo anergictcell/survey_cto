c25ba756cadce6745023303de5aeeaf8aeefa61c
Jojo@UMv3 <rujmarcello@gmail.com>
2016-11-17 15:05:02 +0300

    Addded unit test with spying on request

--- spec/surveyCTO.request.spec.js
+++ spec/surveyCTO.request.spec.js
@@ -1,4 +1,3 @@
-'use strict'
 var Request = require("../lib/Request.js")
 var req = new Request()
 
@@ -20,5 +19,98 @@ describe('SurveyCTO Request Module',function(){
     var servername = "testme"
     req.servername(servername)
     expect(req.servername()).toEqual(servername)
+  })
+})
+
+describe('Checking calls to request module',function(){
+  var failTest = function(error,done) {
+    expect(error).toBeUndefined();
+    done()
+  };
+  
+  beforeEach(function(){
+    spyOn(req,"request").and
+      .callFake(function(options,callback) {
+        callback(null,{statusCode : 200},options)
+      });  
+
+  })
+
+  it("url should be built correctly",function(done){
+    var apiPath = ".surveycto.com/api/v1/forms/data/wide/json/"
+    var servername = "testme"
+    var formId = "myTest"
+    var uri = "https://"+servername+apiPath+formId
+    req
+      .servername(servername)
+      .formId(formId)
+      .get()
+      .then(function(data){
+        expect(data.uri).toEqual(uri)
+        done()
+      })
+      .catch(function(data){failTest(data,done)})
+  })
+
+  it("date should be added to qs",function(done){
+    var lastDate = "Aug 22, 2016 12:09:52 PM"
+    req
+      .lastDate(lastDate)      
+      .get()
+      .then(function(data){
+        expect(data.qs).toEqual({"date":lastDate})
+        done()
+      })
+      .catch(function(data){failTest(data,done)})
+  })
+
+  it("auth should be put correctly",function(done){
+    var user = "tstusr"
+    var password = "tstpwd"
+    var res = { "user": user, "pass": password, "sendImmediately": false }
+    req
+      .auth(user,password)
+      .get()
+      .then(function(data){
+        expect(data.auth).toEqual(res)
+        done()
+      })
+      .catch(function(data){failTest(data,done)})
+  })
+})
+
+describe('Checking errors during network request',function(){
+  it("Server response 401",function(done){
+    spyOn(req,"request").and
+      .callFake(function(options,callback) {
+        callback(null,{statusCode : 401},options)
+      });  
+    req
+      .get()
+      .then(function(data){
+        expect(data).toBeUndefined();
+        done()
+      })
+      .catch(function(error){
+        expect(error).toEqual("Response status code 401")
+        done()
+      })
+  })
+
+  it("Request module error",function(done){
+    spyOn(req,"request").and
+      .callFake(function(options,callback) {
+        callback("Unknown Error","someResponseData","someBody")
+      });  
+    req
+      .get()
+      .then(function(data){
+        expect(data).toBeUndefined();
+        done()
+      })
+      .catch(function(error){
+        expect(error).toEqual("Unknown Error")
+        done()
+      })
   })
 })
